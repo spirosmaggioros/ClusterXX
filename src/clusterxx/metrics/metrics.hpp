016f75ea54cc3dbc9b15d8786ea0d8d132940a23
Spiros Maggioros <spirosastro@gmail.com>
2025-09-02 00:00:28 +0300

    added silhouette score and silhouette samples

--- src/clusterxx/metrics/metrics.hpp
+++ src/clusterxx/metrics/metrics.hpp
@@ -4,6 +4,7 @@
 
 #include <armadillo>
 #include <assert.h>
+#include <float.h>
 #include <math.h>
 
 namespace clusterxx {
@@ -41,7 +42,7 @@ struct chebyshev_distance {
 };
 
 struct manhattan_distance {
-    double operator()(const arma::vec &X, const arma::vec &Y) const {
+    double operator()(const arma::vec &X, const arma::vec &Y = {}) const {
         assert(!X.empty());
         assert(!Y.empty());
         assert(X.n_rows == Y.n_rows);
@@ -55,7 +56,7 @@ struct manhattan_distance {
 
 namespace pairwise_distances {
 struct euclidean_distances {
-    arma::mat operator()(const arma::mat &X, const arma::mat &Y) {
+    arma::mat operator()(const arma::mat &X, const arma::mat &Y = {}) {
         assert(!X.empty());
         arma::mat _Y;
         if (Y.empty()) {
@@ -70,13 +71,12 @@ struct euclidean_distances {
         arma::mat dot_prod = X * _Y.t();
 
         return arma::sqrt(arma::repmat(norm_x, 1, _Y.n_rows) +
-                          arma::repmat(norm_y.t(), X.n_rows, 1) -
-                          2 * dot_prod);
+                          arma::repmat(norm_y.t(), X.n_rows, 1) - 2 * dot_prod);
     }
 };
 
 struct manhattan_distances {
-    arma::mat operator()(const arma::mat &X, const arma::mat &Y) const {
+    arma::mat operator()(const arma::mat &X, const arma::mat &Y = {}) const {
         assert(!X.empty());
 
         arma::mat _Y;
@@ -99,7 +99,7 @@ struct manhattan_distances {
 };
 
 struct squared_euclidean_distances {
-    arma::mat operator()(const arma::mat &X, const arma::mat &Y) const {
+    arma::mat operator()(const arma::mat &X, const arma::mat &Y = {}) const {
         assert(!X.empty());
 
         arma::mat _Y;
@@ -119,7 +119,7 @@ struct squared_euclidean_distances {
 };
 
 struct chebyshev_distances {
-    arma::mat operator()(const arma::mat &X, const arma::mat &Y) const {
+    arma::mat operator()(const arma::mat &X, const arma::mat &Y = {}) const {
         assert(!X.empty());
 
         arma::mat _Y;
@@ -141,6 +141,65 @@ struct chebyshev_distances {
     }
 };
 } // namespace pairwise_distances
+
+namespace clustering {
+template <typename Metric = clusterxx::pairwise_distances::euclidean_distances>
+struct silhouette_samples {
+    std::vector<double> operator()(const arma::mat &X,
+                                   const std::vector<int> &labels) {
+        Metric metric;
+        std::unordered_map<int, int> cluster_size;
+        for (const auto &label : labels) {
+            cluster_size[label]++;
+        }
+
+        std::unordered_map<int, std::vector<double>> dists_point_to_cluster;
+        arma::mat pairwise_dists = metric(X);
+        for (size_t i = 0; i < X.n_rows; i++) {
+            for (size_t j = 0; j < X.n_rows; j++) {
+                if (dists_point_to_cluster.find(i) ==
+                    dists_point_to_cluster.end()) {
+                    dists_point_to_cluster[i].resize(cluster_size.size(), 0.0);
+                }
+                dists_point_to_cluster[i][labels[j]] += pairwise_dists(i, j);
+            }
+        }
+        std::vector<double> A(X.n_rows), B(X.n_rows);
+        for (size_t i = 0; i < X.n_rows; i++) {
+            A[i] = (1.0 / (cluster_size[labels[i]] - 1)) *
+                   dists_point_to_cluster[i][labels[i]];
+            B[i] = DBL_MAX;
+            for (size_t j = 0; j < cluster_size.size(); j++) {
+                if (j == labels[i]) {
+                    continue;
+                }
+                B[i] = std::min(B[i], (1.0 / cluster_size[j]) *
+                                          dists_point_to_cluster[i][j]);
+            }
+        }
+
+        std::vector<double> SC;
+        for (size_t i = 0; i < X.n_rows; i++) {
+            if (cluster_size[labels[i]] > 1) {
+                SC.push_back((B[i] - A[i]) / std::max(A[i], B[i]));
+            } else {
+                SC.push_back(0.0);
+            }
+        }
+
+        return SC;
+    }
+};
+
+template <typename Metric = clusterxx::pairwise_distances::euclidean_distances>
+struct silhouette_score {
+    double operator()(const arma::mat &X, const std::vector<int> &labels) {
+        std::vector<double> SC =
+            clusterxx::clustering::silhouette_samples<Metric>(X, labels);
+        return std::accumulate(SC.begin(), SC.end(), 0.0) / X.n_rows;
+    }
+};
+} // namespace clustering
 } // namespace clusterxx
 
 #endif
